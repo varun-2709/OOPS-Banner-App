3159b43aa92800adb98c0075e73df0374dacfc77
varun-2709 <varun270907@gmail.com>
2026-02-16 08:48:01 +0530

    created oops banner

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -5,11 +5,30 @@
  * asterisks (*) and spaces via multiple print statements.
  *
  * @author Developer
- * @version 1.0
+ * @version 2.0
  */
 
 public class OOPSBannerApp{
     public static void main(String[]args){
-        System.out.println("OOPS"); //displaying OOPS 
+      // Line 1
+        System.out.println("  ***  " + "   " + "  ***  " + "   " + " ******* " + "   " + " ******* ");
+
+        // Line 2
+        System.out.println(" *   * " + "   " + " *   * " + "   " + " *     * " + "   " + " *     * ");
+
+        // Line 3
+        System.out.println("*     *" + " " + "*     *" + " " + " *     * " + "   " + " *     * ");
+
+        // Line 4
+        System.out.println("*     *" + " " + "*     *" + " " + " ******* " + "   " + " ******* ");
+
+        // Line 5
+        System.out.println("*     *" + " " + "*     *" + " " + " *       " + "   " + " *       ");
+
+        // Line 6
+        System.out.println(" *   * " + "   " + " *   * " + "   " + " *       " + "   " + " *       ");
+
+        // Line 7
+        System.out.println("  ***  " + "   " + "  ***  " + "   " + " *       " + "   " + " *       ");
     }
 }
