0c2f08cf3222e72d1ad328a0ce0dee3618d77ff0
varun-2709 <varun270907@gmail.com>
2026-02-16 08:35:39 +0530

    Created OOPS

---
+++ OOPSBannerApp.java
@@ -0,0 +1,15 @@
+/**
+ * OOPSBannerApp UC2 - Banner Display Application
+ *
+ * Displays the word "OOPS" in a banner format using
+ * asterisks (*) and spaces via multiple print statements.
+ *
+ * @author Developer
+ * @version 1.0
+ */
+
+public class OOPSBannerApp{
+    public static void main(String[]args){
+        System.out.println("OOPS"); //displaying OOPS 
+    }
+}
